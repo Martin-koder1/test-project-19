67e73eda1c732bdb2aabf269a4d9885e70d855ba
Martin-koder1 <martinmartinkoder@gmail.com>
2024-12-02 12:00:39 +0100

    input output

--- script.js
+++ script.js
@@ -3,6 +3,80 @@
 // Flat Array:
 const fruitBowl = ["Apple", "Pear", "Grapes"];
 
+// Array methods: 
+fruitBowl.push("Banana");
+fruitBowl.push("Orange");
+fruitBowl.pop();
+
+fruitBowl.unshift("Peach");
+fruitBowl.unshift("Melon");
+fruitBowl.unshift("Tomato");
+fruitBowl.shift();
+
+console.log(fruitBowl);
+console.log(fruitBowl.toString());
+
+
+// Undervisnings oppgave:
+// Lag en variabel som lager en tom array
+// bruk array metodene .push() og .pop() for å legge til data i den tomme arrayen.
+
+/*const suitCase = ["Jacket", "Shoes", "T-shirts"];
+
+suitCase.push("Phone-Charger");
+suitCase.pop();
+suitCase.push("Hat");
+
+console.log(suitCase.toString())*/
+
+
+const vegetableGarden = [];
+vegetableGarden.push("Potet");
+vegetableGarden.push("Gulerot");
+vegetableGarden.push("Kål");
+vegetableGarden.push("Rosenkål");
+vegetableGarden.pop();
+vegetableGarden.push("Ert");
+
+console.log(vegetableGarden);
+
+//
+console.log("___________________________________________")
+//
+
+// Hente inn elementer / Fetch elements
+const inputField = document.querySelector("#inputField");
+const addFront = document.querySelector("#addFront");
+const removeFront = document.querySelector("#removeFront");
+const addBack = document.querySelector("#addBack");
+const removeBack = document.querySelector("#removeBack");
+const outputField = document.querySelector("#outputField");
+
+console.log(inputField, addFront, removeFront, addBack, removeBack, outputField)
+
+const outputArray = [];
+
+// Går elementer hendelser / give elements events
+
+addFront.addEventListener("click", function () {
+  console.log("Button clicked!");
+  
+  outputArray.unshift(inputField.value);
+  
+  console.log(outputArray);
+
+  outputField.textContent = outputArray;
+});
+
+
+
+
+
+
+
+
+
+/*
 // Object with Arrays:
 const biggerFruitBowl = {
   fruit: ["Apple", "Pear", "Grapes"],
@@ -18,3 +92,4 @@ const evenBiggerFruitBowl = {
   ],
   berries: ["Banana", "Strawberry"],
 };
+*/
